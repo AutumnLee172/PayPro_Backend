2152a0e3cb74794afd9a4911dab3f9e32456bcca
AutumnLee172 <78860600+AutumnLee172@users.noreply.github.com>
2023-04-15 16:23:40 +0800

    registration code updated

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -10,37 +10,41 @@ class UserController extends Controller
 {
     public function register(Request $request)
     {
-        $status = false;
+        $created = false;
+        $error = "";
+        try {
+           //check if email existed
+            $email = User::where('email', $request->get('email'))->get()->count();
+            if($email > 0){
+                $created = false;
+                return response()->json([
+                    'status' => true,
+                    'created' => $created,
+                    'error' => "Email was registered before.",
+                ]);
+                return;
+            }
 
-        //check if email existed
-        // $email = User::where('email', $request->get('email'))->get()->count();
-        // if($email > 0){
-        //     return response()->json([
-        //         'status' => true,
-        //         'created' => false,
-        //         'msg' => "Email existed!",
-        //     ]);
-        //     return;
-        // }
+            $user = new User;
+            $user->email = $request->get('email');
+            $user->name = $request->get('name');
+            $user->phone_number = $request->get('phone_number');
+            $user->password = bcrypt($request->get('password'));
+            $user->user_type = "Consumer";
+            if($user->save()){
+                $created = true;
+            }else{
+                $created = false;
+            }  
 
-        $user = new User;
-        $user->email = $request->get('email');
-        $user->name = $request->get('name');
-        $user->phone_number = $request->get('phone_number');
-        $user->password = bcrypt($request->get('password'));
-        $user->user_type = "Consumer";
-        if($user->save()){
-            $status = true;
-        }else{
-            $status = false;
-        }  
-
+        } catch (Throwable $e) {
+           $error = $e;
+        }
+        
         return response()->json([
-            'status' => $status,
-            'created' => true,
-            'data' => [
-                'id' => $user->id
-            ]
+            'status' => true,
+            'created' => $created,
+            'error' => $error,
         ]);
     }
 
